19797f346898f6b68801d18a9b4623f2ecbbee26
FacuBc <facundobesse@gmail.com>
2022-07-31 19:00:42 -0300

    ejercicios

---
+++ src/Coche.java
@@ -0,0 +1,13 @@
+public class Coche {
+
+  private int cantidadPuertas;
+
+  public void addPuerta(int cantidad){
+    this.cantidadPuertas =+ cantidad;
+  }
+
+  public int getCantidadPuertas(){
+    return cantidadPuertas;
+  }
+
+}
---
+++ src/principal.java
@@ -0,0 +1,18 @@
+public class principal {
+
+  public static void main(String[] args) {
+
+    int resultado = sumar(1,2,3);
+    System.out.println(resultado);
+
+    Coche miCoche = new Coche();
+    miCoche.addPuerta(1);
+    System.out.println(miCoche.getCantidadPuertas());
+
+  }
+
+  public static int sumar(int a, int b, int c){
+    return a + b + c;
+  }
+
+}
